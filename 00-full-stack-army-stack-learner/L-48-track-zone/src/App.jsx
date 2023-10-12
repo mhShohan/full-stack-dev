9bb31a5076831cc2b80640063b35373101e02219
mhShohan <mehdihasanshohan25@gmail.com>
2023-10-12 23:46:55 +0600

    convert to UTC timezone

--- 00-full-stack-army-stack-learner/L-48-track-zone/src/App.jsx
+++ 00-full-stack-army-stack-learner/L-48-track-zone/src/App.jsx
@@ -1,7 +1,9 @@
 import ClockList from "./components/clockList/ClockList";
 import LocalClock from "./components/localClock/LocalClock";
+import useClock from "./hooks/useClock";
 
 const App = () => {
+  useClock(new Date());
   return (
     <div>
       <LocalClock />
--- 00-full-stack-army-stack-learner/L-48-track-zone/src/hooks/useClock.jsx
+++ 00-full-stack-army-stack-learner/L-48-track-zone/src/hooks/useClock.jsx
@@ -1,5 +1,45 @@
-const useClock = () => {
-  return;
+import { useEffect, useState } from "react";
+import { addMinutes } from "date-fns";
+
+const init = {
+  id: "",
+  title: "",
+  timezone: { type: "", offset: "" },
+  date_utc: null,
+  date: null,
+};
+
+const TIMEZONE_OFFSET = {
+  PST: -7 * 60,
+  EST: -4 * 60,
+  EDT: -4 * 60,
+  BST: -1 * 60,
+};
+
+const useClock = (timezone, offset = 0) => {
+  const [state, setState] = useState({ ...init });
+  const [utc, setUTC] = useState(null);
+
+  useEffect(() => {
+    let d = new Date();
+    const localOffset = d.getTimezoneOffset();
+    d = addMinutes(d, localOffset);
+    setUTC(d);
+  }, []);
+
+  useEffect(() => {
+    if (utc !== null && timezone) {
+      offset = TIMEZONE_OFFSET[timezone] || offset;
+      const newUTC = addMinutes(utc, offset);
+      setState({ ...state, date_utc: utc, date: newUTC });
+    } else {
+      setState({ ...state, date_utc: utc, date: utc });
+    }
+  }, [utc]);
+
+  return {
+    clock: state,
+  };
 };
 
 export default useClock;
