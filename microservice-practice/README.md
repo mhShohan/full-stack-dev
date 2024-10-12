7b92f5b3047a1e70dd1361470f717ebc88fc0748
mhShohan <mehdihasanshohan25@gmail.com>
2024-10-12 07:30:23 +0600

    microservice-practice from stack learner

---
+++ microservice-practice/README.md
@@ -0,0 +1,6 @@
+# Microservices Practice - video timestamp: 00.39.00
+
+# CLI
+
+- docker ps
+- docker inspect container_name
---
+++ microservice-practice/services/inventory/src/index.ts
@@ -0,0 +1,54 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import cors from 'cors'
+
+const app = express()
+dotenv.config()
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+// environment variables
+const PORT = process.env.PORT || 4002
+const serviceName = process.env.SERVICE_NAME || 'inventory-service'
+
+// health route
+app.get('/health', (_req, res) => {
+  res.status(200).json({
+    status: 'success',
+    statusCode: 200,
+    success: true,
+    message: `${serviceName} is UP now!`
+  })
+})
+
+
+// not Found
+app.use((_req, res) => {
+  res.status(404).json({
+    status: 'failure',
+    statusCode: 404,
+    success: false,
+    message: '404! Not Found.'
+  })
+})
+
+// error handler
+app.use((err, _req, res, _next) => {
+  console.log(err.stack)
+
+  res.status(500).json({
+    status: 'failure',
+    statusCode: 500,
+    success: false,
+    message: 'Internal Server Error!',
+    errors: err
+  })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`${serviceName} running on port: ${PORT}`)
+})
