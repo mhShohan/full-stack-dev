a1647236f0d7aa3ee24930bd63789d21f6cde439
mhShohan <mehdihasanshohan25@gmail.com>
2025-10-31 07:18:16 +0600

    remove medusa projects

--- projects/medusa-playground/medusa-basic-101/basic-101-storefront/src/lib/data/onboarding.ts
+++
@@ -1,9 +0,0 @@
-"use server"
-import { cookies as nextCookies } from "next/headers"
-import { redirect } from "next/navigation"
-
-export async function resetOnboardingState(orderId: string) {
-  const cookies = await nextCookies()
-  cookies.set("_medusa_onboarding", "false", { maxAge: -1 })
-  redirect(`http://localhost:7001/a/orders/${orderId}`)
-}
